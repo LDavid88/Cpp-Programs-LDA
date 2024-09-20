7eda4d39bdec74d772275acfd8c02a1adccaaa4e
David Romero <pollinxld@gmail.com>
2024-09-19 21:46:40 -0600

    add age input

--- input.cpp
+++ input.cpp
@@ -4,11 +4,16 @@
 // cin ----> console input
 int main() {
     std::string name;
+    int age;
 
     std::cout << "What is your name?: ";
     std::cin >> name;
 
-    std::cout << "Hello " << name;
+    std::cout << "What is your age?: ";
+    std::cin >> age;
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "This is your age " << age;
 
     return 0;
 }
