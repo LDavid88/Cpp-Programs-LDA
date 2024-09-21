310012a80e1929ce5ef82ca5ae81514b28cd0db1
David Romero <pollinxld@gmail.com>
2024-09-20 20:23:24 -0600

    add math functions

--- input.cpp
+++ input.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cmath>
 
 // cout ---> console output
 // cin ----> console input
+/*
 int main() {
     std::string name;
     int age;
@@ -14,6 +16,26 @@ int main() {
 
     std::cout << "Hello " << name << '\n';
     std::cout << "This is your age " << age;
+
+    return 0;
+}
+*/
+
+int main() {
+    double x = 3.1;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x, y);
+    //z = pow(2, 4);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(x);
+    //z = ceil(x);
+    z = floor(x);
+
+    std::cout << z;
 
     return 0;
 }
